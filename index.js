d43b0c963f65d2c6ad0169b06192c0d903e6a40e
RebeccaEvans <evans.rebeccaj@gmail.com>
2019-12-12 18:39:09 -0800

    added geocode function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var layouts = require('express-ejs-layouts');
 var parser = require('body-parser');
+var geocoder = require('simple-geocoder');
 
 // Declare your app
 var app = express();
@@ -14,12 +15,24 @@ app.use(layouts);
 app.use(express.static('static'));
 app.use(parser.urlencoded({ extended: false }));
 
+
 // Declare routes
 app.get('/', function(req, res){
   res.render('home');
 });
 
+
 app.post('/', function(req, res){
+  let location = req.body.address
+
+  geocoder.geocode(location, function(success, locations) {
+    if(success) {
+      var longitude = locations.x
+      var latitude = locations.y
+      console.log("Location: ", longitude, latitude);
+    }
+
+  });
   res.render('result');
 });
 
